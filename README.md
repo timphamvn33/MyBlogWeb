69f00128dc545797a9798d33c205838b1df3d693
Thuong Pham <timphamvn33@gmail.com>
2022-01-29 23:03:52 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_blog
+here is the link of my blog website: https://young-eyrie-73686.herokuapp.com/
---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+// create object mongoose
+const mongoose =require ('mongoose');
+
+const homeStartingContent = "Hello! Lets Start Your Daily Journal ";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+// conect mongoose
+mongoose.connect('mongodb+srv://timphamvn33:Thuonghuyen55@cluster0.fo64y.mongodb.net/myBlog');
+// create the schema 
+const blogSchema = new mongoose.Schema({
+  title: String, 
+  content: String
+})
+
+// compile schema to model
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+  
+app.get("/", function(req, res){
+  
+  Blog.find({}, function(err, foundBlog){
+    res.render("home", {home: "Home", content: homeStartingContent, posts: foundBlog})
+  })
+
+})
+
+// app.get("/contact", function(req, res){
+//   res.render("contact", {contact_content: contactContent})
+// })
+// app.get("/about", function(req, res){
+//   res.render("about", {about_content: aboutContent})
+// })
+app.get("/compose", function(req, res){
+  res.render("compose")
+
+})
+app.get("/post/:postId", function(req, res){
+  
+  const requestPostId= req.params.postId
+  
+  Blog.findOne({_id: requestPostId }, function(err, foundPost){
+    res.render("post", {title: foundPost.title, content: foundPost.content})
+  })
+  
+ 
+  
+})
+// save the title and content after the publish is trigered
+app.post("/compose", function(req, res){
+  const post = new Blog({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  })
+  // add the call back function to save the data and go back to home page 
+    post.save()
+      res.redirect("/")
+   
+
+
+  
+
+})
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
